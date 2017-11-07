328192edfb842f54e497e1fd09ca1f76c8319dcb
Pedro <pedrobest.m@gmail.com>
2017-11-07 22:26:44 +0000

    Background v2

--- src/ClientSide/Image.java
+++ src/ClientSide/Image.java
@@ -2,6 +2,8 @@ package ClientSide;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
+import java.net.URL;
 
 public class Image extends JFrame {
 
@@ -13,8 +15,13 @@ public class Image extends JFrame {
     JButton botao;
     JLabel label;
 
+
     public Image() {
 
+        //LOAD DA IMAGEM
+
+        ClassLoader classLoader = getClass().getClassLoader();
+        String file = classLoader.getResource("images/BattleShip.png").getPath();
 
         setTitle("BattleShip");
         setSize(1280, 720);
@@ -23,7 +30,7 @@ public class Image extends JFrame {
         setVisible(true);
         setLayout(new BorderLayout());
         JLabel background = new JLabel();
-        background.setIcon(new ImageIcon("C:\\Users\\Pedro\\Desktop\\BattleShip.png"));
+        background.setIcon(new ImageIcon(file));
         background.setLayout(new FlowLayout());
         background.setSize(1300,760);
         background.setLocation(0,0);
