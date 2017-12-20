5df136c02cc697aa7849d858a4f3e177d0f34854
David Pinto <daxtery11@gmail.com>
2017-12-20 14:26:10 +0000

    AI - done NOW

--- Tests/Common/PlayerBoardTest.java
+++ Tests/Common/PlayerBoardTest.java
@@ -117,15 +117,15 @@ class  PlayerBoardTest {
         pb.getAttacked(5,3);
         pb.getAttacked(5,2);
 
-        System.out.println(pb);
+        //System.out.println(pb);
 
         pb = new PlayerBoard(pb.getToPaint());
 
-        System.out.println("---------------------------------");
-        System.out.println("---------------------------------");
-        System.out.println("---------------------------------");
+        //System.out.println("---------------------------------");
+        //System.out.println("---------------------------------");
+        //System.out.println("---------------------------------");
 
-        System.out.println(pb);
+        //System.out.println(pb);
 
 
     }
